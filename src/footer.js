57b58d44b917b1f10cb0e9f51a87cbf9b227c108
harshalDev <harshalwarade344@gmail.com>
2024-09-29 00:27:40 +0530

    add files

---
+++ src/footer.js
@@ -0,0 +1,45 @@
+document.write(`
+        <footer class="bg-gray-200 text-gray-800 pt-10">
+      <div class="container mx-auto grid grid-cols-2 md:grid-cols-4 gap-8 px-6">
+        <div class="footer-section">
+          <h4 class="text-xl font-semibold mb-4">Company Name</h4>
+          <p>Tag Line</p>
+        </div>
+        <div class="footer-section">
+          <h4 class="text-xl font-semibold mb-4">Products</h4>
+          <ul>
+            <li><a href="#" class="hover:text-gray-600">CRM</a></li>
+          </ul>
+        </div>
+        <div class="footer-section">
+          <h4 class="text-xl font-semibold mb-4">Services</h4>
+          <ul>
+            <li>
+              <a href="#" class="hover:text-gray-600">Pre-build Solution</a>
+            </li>
+            <li>
+              <a href="#" class="hover:text-gray-600">Digital Strategy</a>
+            </li>
+            <li><a href="#" class="hover:text-gray-600">Custom Solution</a></li>
+            <li><a href="#" class="hover:text-gray-600">Web Development</a></li>
+            <li><a href="#" class="hover:text-gray-600">App Development</a></li>
+          </ul>
+        </div>
+        <div class="footer-section">
+          <h4 class="text-xl font-semibold mb-4">Company</h4>
+          <ul>
+            <li>
+              <a href="#" class="hover:text-gray-600">Terms and Conditions</a>
+            </li>
+            <li><a href="#" class="hover:text-gray-600">Privacy Policy</a></li>
+            <li><a href="#" class="hover:text-gray-600">Careers</a></li>
+            <li><a href="#" class="hover:text-gray-600">About Us</a></li>
+            <li><a href="#" class="hover:text-gray-600">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-bottom mt-8 h-[40px] text-center bg-[#0068b5] flex items-center justify-center text-white">
+        <p>Made with ❤️ from India © 2024 Company Name</p>
+      </div>
+    </footer>    
+`);
---
+++ src/navbar.js
@@ -0,0 +1,59 @@
+document.write(`
+<header class="bg-white text-gray-800">
+<div
+  class="container mx-auto flex items-center justify-between py-4 px-2"
+>
+  <div class="text-xl font-bold">Logo</div>
+
+  <nav class="hidden md:flex space-x-6">
+    <a href="index.html" class="hover:text-gray-600">Home</a>
+    <a href="#" class="hover:text-gray-600">Company</a>
+    <a href="raiseaticket.html" class="hover:text-gray-600">Raise Ticket</a>
+    <a href="contact.html" class="hover:text-gray-600">Contact</a>
+  </nav>
+
+  <button
+    class="hidden md:inline-block bg-[#0068B5] rounded-sm text-white px-4 py-2"
+  >
+    BOOK A DEMO
+  </button>
+
+  <button
+    class="md:hidden text-gray-800 focus:outline-none"
+    id="mobileMenuBtn"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 6h16M4 12h16M4 18h16"
+      />
+    </svg>
+  </button>
+</div>
+
+<nav class="md:hidden bg-white text-gray-800 hidden" id="mobileMenu">
+  <ul class="flex flex-col space-y-4 p-4">
+    <li><a href="index.html" class="hover:text-gray-600">Home</a></li>
+    <li><a href="#" class="hover:text-gray-600">Company</a></li>
+    <a href="raiseaticket.html" class="hover:text-gray-600">Raise Ticket</a>
+    <a href="contact.html" class="hover:text-gray-600">Contact</a>
+    <li>
+      <button
+        class="bg-[#0068B5] rounded-sm text-white px-4 py-2"
+      >
+        BOOK A DEMO
+      </button>
+    </li>
+  </ul>
+</nav>
+</header>    
+    
+`);
